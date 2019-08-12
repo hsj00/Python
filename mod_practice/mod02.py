8448ccbad0fb1202128e0a9b7b4fa20a38b3747f
Seung-joo Han <anartanimal@Seung-jooui-MacBook-Air.local>
2019-08-12 23:01:38 +0900

    CH05, module

---
+++ mod_practice/mod02.py
@@ -0,0 +1,10 @@
+# class/var include
+# mod02.py
+
+PI = 3.141592
+class Math:
+    def solv(self, r):
+        return PI * (r ** 2)
+
+def add(a, b): 
+    return a + b
---
+++ mod_practice/modtest.py
@@ -0,0 +1,4 @@
+# modtest.py
+import mod02
+result = mod02.add(3, 4)
+print(result)
